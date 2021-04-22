d6ee203c86890b71f6a8639da8d72d09ef079bcc
Lloyd <lloydcox13@aol.com>
2021-04-22 18:54:21 +0100

    First comitt
    Initial push, everything is built and works, doesn't look the prettiest but does what Will has asked. Will try and improve on it.

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <title>Will's Anagram Task</title>
+        <meta name="NetMatters" content="NetMatters Home Page">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="CSS/main.css" rel="stylesheet" type="text/css"> 
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"> 
+        </head>
+    <body>
+        <header>
+            <h1>Will's Anagram Challenge</h1>
+        </header>
+        <div class="container">
+            <form class="input-form" method="post">
+                <div class="input-div">
+                    <label>Please type a word or string of letters here.</label>
+                        <input id="input1" type="text" name="input1" class="input">
+                            <?php
+                            if (isset($_POST['input1'])) {
+                                    $word1 = true;
+                                } 
+                            ?>
+                </div>
+                <div class="input-div">    
+                    <label>Please type a word or string of letters here.</label>
+                        <input id="input2" type="text" name="input2" class="input">
+                            <?php
+                            if (isset($_POST['input2'])) {
+                                    $word2 = true;
+                                } 
+                            ?>
+                </div>
+                    <button type="submit" name="button" class="button" value="button">Check for Anagram</button>
+                </form>
+            <?php 
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                    $word1 = $_POST['input1'];
+                    $word2 = $_POST['input2'];
+
+                
+                    function isAnagram($string1, $string2)
+                    {
+                        // quick check, eliminate obvious mismatches quickly
+                        if (strlen($string1) != strlen($string2)) {
+                            return false;
+                        }
+                    
+                        // Handle uppercase to lowercase comparisons
+                        $array1 = count_chars(strtolower($string1));
+                        $array2 = count_chars(strtolower($string2));
+                    
+                        // Check if 
+                        if (!empty(array_diff_assoc($array2, $array1))) {
+                            return false;
+                        } 
+                        if (!empty(array_diff_assoc($array1, $array2))) {
+                            return false;
+                        } 
+                    
+                        return true;
+                    }
+
+                    if (isAnagram($word1, $word2)) {
+                        echo "${word1} is an anagram of ${word2}";
+                    } else {
+                        echo "${word1} is not an anagram of ${word2}";
+                    }
+                }
+            ?>
+        </div>
+ 
+    </body>
+</html
