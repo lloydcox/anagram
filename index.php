3e47fa778960123df3dd6ddb031a8b16bcfb8b87
Lloyd <lloydcox13@aol.com>
2021-04-26 09:14:16 +0100

    Minor update
    Name update where I didn't change the meta name from using the boiler template.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <head>
         <meta charset="utf-8">
         <title>Will's Anagram Task</title>
-        <meta name="NetMatters" content="NetMatters Home Page">
+        <meta name="Will's Anagram Task" content="NetMatters Home Page">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link href="CSS/main.css" rel="stylesheet" type="text/css"> 
